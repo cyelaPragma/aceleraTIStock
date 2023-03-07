9f53509df8f0b8cd7e9e8ce2dc4060cb7a18e912
hgarciaospina <henrygarciaospina@gmail.com>
2023-03-06 20:03:11 -0500

    Se cambio el tipo de dato del Id de los models Provision y Suplier de int a Long

--- src/main/java/com/acelerati/stock/domain/model/stock/Provision.java
+++ src/main/java/com/acelerati/stock/domain/model/stock/Provision.java
@@ -10,7 +10,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class Provision {
-    private int id;
+    private Long id;
     private Date date;
     private int userId;
     private int stock;
--- src/main/java/com/acelerati/stock/domain/model/stock/Supplier.java
+++ src/main/java/com/acelerati/stock/domain/model/stock/Supplier.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Supplier {
-    private int id;
+    private Long id;
     private String name;
     private String description;
 }
